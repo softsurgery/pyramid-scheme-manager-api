bcd9c7ca61e11546fcbc92e8e2cefc982c1919a1
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2024-07-22 18:58:14 +0100

    first commit

---
+++ src/main/java/com/pyramidscheme/mainartifact/MainartifactApplication.java
@@ -0,0 +1,13 @@
+package com.pyramidscheme.mainartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainartifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainartifactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pyramidscheme/mainartifact/MainartifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyramidscheme.mainartifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainartifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
